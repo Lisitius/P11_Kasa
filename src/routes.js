0c1d7d5a23ddaedabbb0e7c3bdc0982d781f35e0
unknown <benjamin.fichaux@gmail.com>
2023-04-26 14:40:42 +0200

    first commit react, react-router-dom, sass & little config

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <Switch>
+      {/* Routes */}
+      {/* Routes */}
+      {/* Routes */}
+    </Switch>
+  );
+};
+
+export default Routes;
