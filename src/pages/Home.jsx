4fd8dcbf627fb14994d176adf1216e27739417e8
unknown <benjamin.fichaux@gmail.com>
2023-04-27 15:17:30 +0200

    banner home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import Banner from "../layouts/banner/Banner";
 import "./Home.scss";
 
 const Home = () => {
-  return <div className="home-container"></div>;
+  return (
+    <div className="home-container">
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
